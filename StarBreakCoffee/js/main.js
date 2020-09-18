cd0035ebae62eea06884cde06cadb7831cd16873
Daisy Qin <dqin@onyxgs.com>
2020-09-18 09:52:44 -0400

    add data

--- StarBreakCoffee/js/main.js
+++ StarBreakCoffee/js/main.js
@@ -4,3 +4,20 @@
 *    Project 1 - Star Break Coffee
 */
 
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 }
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+
+const svg = d3.select("#chart-area")
+  .append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+d3.csv("data/revenues.csv").then(data => {
+  data.forEach(d => {
+    d.revenue = +d.revenue;
+    d.profit = +d.profit
+  })
+  console.log(data)
+})
+
