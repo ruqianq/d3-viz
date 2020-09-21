b18c137985dae5d40df415f7320e974728194994
Daisy Qin <dqin@onyxgs.com>
2020-09-21 10:30:10 -0400

    add the update function and transition

--- StarBreakCoffee/js/main.js
+++ StarBreakCoffee/js/main.js
@@ -4,14 +4,13 @@
 *    Project 1 - Star Break Coffee
 */
 
-const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 }
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 
 let flag = true
 
-const svg = d3.select("#chart-area")
-  .append("svg")
+const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
@@ -27,6 +26,7 @@ g.append("text")
   .attr("text-anchor", "middle")
   .text("Month")
 
+// Y label
 const yLabel = g.append("text")
   .attr("class", "y axis-label")
   .attr("x", - (HEIGHT / 2))
@@ -35,13 +35,13 @@ const yLabel = g.append("text")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
 
-const y = d3.scaleLinear()
-  .range([HEIGHT, 0])
-
 const x = d3.scaleBand()
   .range([0, WIDTH])
   .paddingInner(0.3)
   .paddingOuter(0.2)
+
+const y = d3.scaleLinear()
+  .range([HEIGHT, 0])
 
 const xAxisGroup = g.append("g")
   .attr("class", "x axis")
@@ -52,13 +52,12 @@ const yAxisGroup = g.append("g")
 
 d3.csv("data/revenues.csv").then(data => {
   data.forEach(d => {
-    d.revenue = +d.revenue;
-    d.profit = +d.profit
+    d.revenue = Number(d.revenue)
+    d.profit = Number(d.profit)
   })
 
   d3.interval(() => {
     flag = !flag
-    // hard code part
     const newData = flag ? data : data.slice(1)
     update(newData)
   }, 1000)
@@ -74,8 +73,7 @@ function update(data) {
   y.domain([0, d3.max(data, d => d[value])])
 
   const xAxisCall = d3.axisBottom(x)
-  xAxisGroup.transition(t)
-    .call(xAxisCall)
+  xAxisGroup.transition(t).call(xAxisCall)
     .selectAll("text")
     .attr("y", "10")
     .attr("x", "-5")
@@ -85,15 +83,14 @@ function update(data) {
   const yAxisCall = d3.axisLeft(y)
     .ticks(5)
     .tickFormat(d => "$" + d)
-  yAxisGroup.transition(t)
-    .call(yAxisCall)
+  yAxisGroup.transition(t).call(yAxisCall)
 
   // JOIN new data with old elements.
-  const revenueBars = g.selectAll("rect")
+  const rects = g.selectAll("rect")
     .data(data, d => d.month)
 
   // EXIT old elements not present in new data.
-  revenueBars.exit()
+  rects.exit()
     .attr("fill", "red")
     .transition(t)
     .attr("height", 0)
@@ -101,7 +98,7 @@ function update(data) {
     .remove()
 
   // ENTER new elements present in new data...
-  revenueBars.enter().append("rect")
+  rects.enter().append("rect")
     .attr("fill", "grey")
     .attr("y", y(0))
     .attr("height", 0)
