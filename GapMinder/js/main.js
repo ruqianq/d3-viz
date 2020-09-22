38c100ee2d40d03e9527d88bcc939506ca2a0a08
Daisy Qin <dqin@onyxgs.com>
2020-09-22 09:41:03 -0400

    add the interval function to increase the year

--- GapMinder/js/main.js
+++ GapMinder/js/main.js
@@ -8,6 +8,8 @@ const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 
+let year = 0
+
 const svg = d3.select("#chart-area").append("svg")
 	.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
 	.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
@@ -16,10 +18,6 @@ const g = svg.append("g")
 	.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
 d3.json("data/data.json").then(function(data){
-	const firstYearData = data[0]['countries'].filter((c) => (c.income !== null && c.life_exp !== null
-		&& c.population !== null
-	))
-
 	const allData = data.map((y) => {
 		return y.countries.filter((c) => (c.income !== null && c.life_exp !== null
 			&& c.population !== null
@@ -27,7 +25,7 @@ d3.json("data/data.json").then(function(data){
 	})
 
 	d3.interval(() => {
-
+		year += 1
 	}, 1000)
 
 })
