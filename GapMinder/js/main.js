7f17a3807aa2248f8f427d62cd2ffe72dc343099
Daisy Qin <dqin@onyxgs.com>
2020-09-22 10:49:02 -0400

    add the color

--- GapMinder/js/main.js
+++ GapMinder/js/main.js
@@ -21,6 +21,7 @@ const g = svg.append("g")
 const x = d3.scaleLog().base(10).range([0, WIDTH]).domain([142, 150000])
 const y = d3.scaleLinear().range([HEIGHT, 0]).domain([0, 90])
 const area = d3.scaleLinear().range([25 * Math.PI, 1500 * Math.PI]).domain([2000, 1400000000])
+const continentColor = d3.scaleOrdinal(d3.schemePastel1)
 
 d3.json("data/data.json").then(function (data) {
   const allData = data.map((y) => {
@@ -43,7 +44,7 @@ function update(data) {
 
   circles.enter()
     .append("circle")
-    .attr("fill", "red")
+    .attr("fill", (d) => continentColor(d.continent))
     .attr("cy", (d) => y(d.life_exp))
     .attr("r", d => Math.sqrt(area(d.population) / Math.PI))
     .attr("cx", (d) => x(d.income))
