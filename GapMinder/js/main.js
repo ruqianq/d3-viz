ffda24bb5a013e0f28a6f343ab874cbcdecd6962
Daisy Qin <dqin@onyxgs.com>
2020-09-22 08:48:17 -0400

    add the update function and transition

--- GapMinder/js/main.js
+++ GapMinder/js/main.js
@@ -4,6 +4,17 @@
 *    Project 2 - Gapminder Clone
 */
 
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+
+const svg = d3.select("#chart-area").append("svg")
+	.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+	.attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+
+
 d3.json("data/data.json").then(function(data){
 	console.log(data);
 })
