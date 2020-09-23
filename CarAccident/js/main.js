484b8ea1231532b2c2fb4a945e9764c8904d3ee7
Daisy Qin <dqin@onyxgs.com>
2020-09-23 08:49:56 -0400

    create the bar chart

--- CarAccident/js/main.js
+++ CarAccident/js/main.js
@@ -8,6 +8,8 @@ const MARGIN = {LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100}
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 
+let flag = true
+
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
@@ -33,17 +35,45 @@ const yLabel = g.append("text")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
 
-const x = d3.scaleBand()
-  .range([0, WIDTH])
-  .paddingInner(0.3)
-  .paddingOuter(0.2)
-
-const y = d3.scaleLinear()
-  .range([HEIGHT, 0])
-
 d3.csv("data/vehicle_accidents.csv").then(data => {
   data.forEach(d => d.Frequency = Number(d.Frequency))
   let testData = data.filter((d) => {return d.vehicle_make_name === "Acura"}
   )
-  console.log(testData)
+  const x = d3.scaleBand()
+    .domain(testData.map(d => d.sas_mnth_of_crsh))
+    .range([0, WIDTH])
+    .paddingInner(0.3)
+    .paddingOuter(0.2)
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(testData, d => d.Frequency)])
+    .range([HEIGHT, 0])
+
+  const xAxisCall = d3.axisBottom(x)
+  g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${HEIGHT})`)
+    .call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)")
+
+  const yAxisCall = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d)
+  g.append("g")
+    .attr("class", "y axis")
+    .call(yAxisCall)
+
+  const rects = g.selectAll("rect")
+    .data(testData)
+
+  rects.enter().append("rect")
+    .attr("y", d => y(d.Frequency))
+    .attr("x", (d) => x(d.sas_mnth_of_crsh))
+    .attr("width", x.bandwidth)
+    .attr("height", d => HEIGHT - y(d.Frequency))
+    .attr("fill", "grey")
 })
