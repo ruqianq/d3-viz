b9802c9d3db734bbb4dca09999231b9ce380f9dd
Daisy Qin <dqin@onyxgs.com>
2020-09-23 09:17:25 -0400

    get the bar chart working

--- CarAccident/js/main.js
+++ CarAccident/js/main.js
@@ -35,45 +35,63 @@ const yLabel = g.append("text")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
 
+const x = d3.scaleBand()
+  .range([0, WIDTH])
+  .paddingInner(0.3)
+  .paddingOuter(0.2)
+
+const y = d3.scaleLinear()
+  .range([HEIGHT, 0])
+
+// X Axis
+const xAxisCall = d3.axisBottom(x)
+
+g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, ${HEIGHT})`)
+  .call(xAxisCall)
+
+// Y Axis
+const yAxisCall = d3.axisLeft(y)
+g.append("g")
+  .attr("class", "y axis")
+  .call(yAxisCall)
+
 d3.csv("data/vehicle_accidents.csv").then(data => {
   data.forEach(d => d.Frequency = Number(d.Frequency))
-  let testData = data.filter((d) => {return d.vehicle_make_name === "Acura"}
-  )
-  const x = d3.scaleBand()
-    .domain(testData.map(d => d.sas_mnth_of_crsh))
-    .range([0, WIDTH])
-    .paddingInner(0.3)
-    .paddingOuter(0.2)
+  let testData = data.filter((d) => {
+    return d.vehicle_make_name === "Acura" && d.sas_yr_of_crsh === "2015"
+  })
 
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(testData, d => d.Frequency)])
-    .range([HEIGHT, 0])
+  d3.interval(() => {
+    flag = !flag
+    const newData = flag ? testData : data.filter((d) => {
+      return d.vehicle_make_name === "Acura" && d.sas_yr_of_crsh === "2016"
+    })
+    update(newData)
+  }, 1000)
 
-  const xAxisCall = d3.axisBottom(x)
-  g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0, ${HEIGHT})`)
-    .call(xAxisCall)
-    .selectAll("text")
-    .attr("y", "10")
-    .attr("x", "-5")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-40)")
+  update(testData)
+})
 
-  const yAxisCall = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d)
-  g.append("g")
-    .attr("class", "y axis")
-    .call(yAxisCall)
+function update(data) {
+  const t = d3.transition().duration(750)
+  x.domain(data.map(d => d.sas_mnth_of_crsh))
+  y.domain([0, d3.max(data, d => d.Frequency)])
 
   const rects = g.selectAll("rect")
-    .data(testData)
+    .data(data, d => d.sas_mnth_of_crsh)
+
+  rects.exit().remove()
 
   rects.enter().append("rect")
-    .attr("y", d => y(d.Frequency))
+    .attr("fill", "grey")
     .attr("x", (d) => x(d.sas_mnth_of_crsh))
     .attr("width", x.bandwidth)
+    // AND UPDATE old elements present in new data.
+    .merge(rects)
+    .transition(t)
+    .attr("y", d => y(d.Frequency))
     .attr("height", d => HEIGHT - y(d.Frequency))
-    .attr("fill", "grey")
-})
+
+}
