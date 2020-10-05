f345693536044af0f5677a02a9b2d395b290fcb4
Daisy Qin <dqin@onyxgs.com>
2020-10-05 15:19:10 -0400

    add the svg tag and group

--- CarAccident/PedFatalityByMonth/js/main.js
+++ CarAccident/PedFatalityByMonth/js/main.js
@@ -2,6 +2,13 @@ const margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
 const parseTime = d3.timeParse("%B %-d, %Y %H:%M:%S %p");
 
 // set the ranges
@@ -14,6 +21,9 @@ d3.csv("data/sas_test_data.csv").then(function(data) {
   data.forEach(function (d) {
     d.date = parseTime(d.sas_tmstmp_of_crsh);
     d.number_of_fatalities = +d.number_of_fatalities
-    console.log(d)
   });
+
+  const rects = g.selectAll("rect")
+    .data(data, d => d.number_of_fatalities)
+
 })
