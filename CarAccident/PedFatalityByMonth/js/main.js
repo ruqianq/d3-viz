006e98aa399742e0404f80297d1d11fb5205a37f
Daisy Qin <dqin@onyxgs.com>
2020-10-06 10:59:28 -0400

    adding clean data

--- CarAccident/PedFatalityByMonth/js/main.js
+++ CarAccident/PedFatalityByMonth/js/main.js
@@ -9,40 +9,12 @@ const svg = d3.select("#chart-area").append("svg")
 const g = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-const parseTime = d3.timeParse("%B %-d, %Y %H:%M:%S %p");
-
 // set the ranges
 var x = d3.scaleBand().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
 d3.csv("data/sas_test_data.csv").then(function(data) {
 
-  // format the data
-  data.forEach(function (d) {
-    d.sas_tmstmp_of_crsh = parseTime(d.sas_tmstmp_of_crsh);
-    d.month = d.sas_tmstmp_of_crsh.getMonth()
-    d.year = d.sas_tmstmp_of_crsh.getFullYear()
-    d.number_of_fatalities = +d.number_of_fatalities
-  });
-  let result = []
 
-  let p = 0;
-  let acc = 0
-  while (p < data.length - 1) {
-    acc += data[p].number_of_fatalities
-
-    if (data[p].month !== data[p+1].month){
-      let fatalByMonth = {}
-      fatalByMonth["sum"] = acc
-      fatalByMonth["month"] = data[p].month
-      fatalByMonth["year"] = data[p].year
-      result.push(fatalByMonth)
-      acc = 0
-    }
-    p++
-  }
-
-  // const rects = g.selectAll("rect")
-  //   .data(data, d => d.number_of_fatalities)
 
 })
