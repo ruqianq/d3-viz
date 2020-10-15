f0ca70590d02d6abfe543e9a1eb1286d89977f1d
Daisy Qin <dqin@onyxgs.com>
2020-10-15 12:16:58 -0400

    add the new data

--- CoinStats/js/main.js
+++ CoinStats/js/main.js
@@ -16,9 +16,9 @@ const g = svg.append("g")
   .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
 // time parser for x-scale
-const parseTime = d3.timeParse("%Y")
+const parseTime = d3.timeParse("%d/%m/%Y")
 // for tooltip
-const bisectDate = d3.bisector(d => d.year).left
+const bisectDate = d3.bisector(d => d.date).left
 
 // scales
 const x = d3.scaleTime().range([0, WIDTH])
@@ -49,18 +49,23 @@ yAxis.append("text")
 
 // line path generator
 const line = d3.line()
-	.x(d => x(d.year))
+	.x(d => x(d.date))
 	.y(d => y(d.value))
 
-d3.json("data/example.json").then(data => {
-	// clean data
-	data.forEach(d => {
-		d.year = parseTime(d.year)
+d3.json("data/coins.json").then(data => {
+
+	let bitcoin = data["bitcoin"]
+
+	bitcoin.forEach(d => {
+		d.date = parseTime(d.date)
 		d.value = Number(d.value)
+
 	})
+	console.log(bitcoin)
+
 
 	// set scale domains
-	x.domain(d3.extent(data, d => d.year))
+	x.domain(d3.extent(data, d => d.date))
 	y.domain([
 		d3.min(data, d => d.value) / 1.005, 
 		d3.max(data, d => d.value) * 1.005
@@ -114,11 +119,11 @@ d3.json("data/example.json").then(data => {
 		const i = bisectDate(data, x0, 1)
 		const d0 = data[i - 1]
 		const d1 = data[i]
-		const d = x0 - d0.year > d1.year - x0 ? d1 : d0
-		focus.attr("transform", `translate(${x(d.year)}, ${y(d.value)})`)
+		const d = x0 - d0.date > d1.date - x0 ? d1 : d0
+		focus.attr("transform", `translate(${x(d.date)}, ${y(d.value)})`)
 		focus.select("text").text(d.value)
 		focus.select(".x-hover-line").attr("y2", HEIGHT - y(d.value))
-		focus.select(".y-hover-line").attr("x2", -x(d.year))
+		focus.select(".y-hover-line").attr("x2", -x(d.date))
 	}
 	
 	/******************************** Tooltip Code ********************************/
